e969854c691ad333e6eea377691332bcec83d5df
TarasOzarkiv <tarasozarkiv@gmail.com>
2023-12-02 23:11:26 -0500

    day 4

---
+++ src/test/java/com/loop/test/day3_locators/T1_locators_getText.java
@@ -0,0 +1,20 @@
+package com.loop.test.day3_locators;
+
+import com.loop.test.utilities.DocuportConstants;
+import com.loop.test.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class T1_locators_getText {
+    public static void main(String[] args) {
+
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.get("https://beta.docuport.app");
+
+        WebElement userName = driver.findElement(By.id("input-14"));
+        userName.sendKeys(DocuportConstants.USERNAME_CLIENT);
+
+    }
+}
