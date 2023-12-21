b0980c1af256cab8195b7382b5cd95a56291dfea
TarasOzarkiv <tarasozarkiv@gmail.com>
2023-12-21 10:20:07 -0500

    day 8

--- src/test/java/com/loop/test/utilities/BrowserUtils.java
+++ src/test/java/com/loop/test/utilities/BrowserUtils.java
@@ -27,4 +27,22 @@ public class BrowserUtils {
         assertTrue(driver.getTitle().toLowerCase().contains(expectedTitle));
     }
 
+    /**
+     * switches to the new window by the exact title
+     * return to original window if the window with given title not found
+     * @param driver
+     * @param targetTitle
+     * @author Taras Ozarkiv
+     */
+    public static void switchToWindow(WebDriver driver, String targetTitle){
+        String origin = driver.getWindowHandle();
+        for (String handle : driver.getWindowHandles()){
+            driver.switchTo().window(handle);
+            if(driver.getTitle().contains(targetTitle)){
+                return;
+            }
+        }
+        driver.switchTo().window(origin);
+    }
+
 }
